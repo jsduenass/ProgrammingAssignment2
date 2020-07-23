9386dfbbe1261d6ceac2dbff875e9f2859b557b0
jsduenass <jsduenass@unal.edu.co>
2020-07-23 18:23:51 -0500

    createing a cached matrix inverse system
    First try at solving the assigment of creating functions  ment to cached the Inverse of a given matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+#--------------------------------------
+#           Solution
+#--------------------------------------
 
-## Write a short comment describing this function
+## These functions are ment to cached the Inverse of a given matrix, 
+## taking for granted that the input is a proper square invertible matrix
+## no error handling has been taken into account  
+
+
+## creates a cached matrix Object: a colletion of functions from which 
+## the matrix and inverse values can be acces or set up 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    Inv <- NULL
+    set <- function(value) {
+        x <<- value
+        m <<- NULL
+    }
+    get <- function() x
+    
+    setInverse <- function(Inv_value) Inv <<- Inv_value
+    getInverse <- function() Inv
+    
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+    
 }
 
 
-## Write a short comment describing this function
+## takes as input a cached matrix Object,
+## gets the value of the cached inverse, if the value has been calculated  
+## it gets returned, if it hasn't been calculated the inverse is solved 
+## and it's value stored inside the cached matrix Object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    Inv <- x$getInverse()
+    if(!is.null(Inv)) {
+        message("inverse has alredy been calculated not need to do it again")
+        message("here you go")
+        return(Inv)
+    }
+    data <- x$get()
+    message("first time calculating the inverse")
+    message("it might take a while")
+    
+    Inv<- solve(data, ...)
+    x$setInverse(Inv)
+    message("here you go")
+    
+    Inv
 }
+
+
+#--------------------------------------
+#           Example
+#--------------------------------------
+
+#M<- matrix(c(1,2,3,4),2)
+
+#M_Obj <- makeCacheMatrix(M)
+
+#ans<-cacheSolve(M_Obj)
+#ans2<-cacheSolve(M_Obj)
